2fc599d66c466c645a64681d59daf7eaf14382d0
Aidan Hibbard <aidanchibbard@gmail.com>
2023-05-14 11:17:34 -0700

    Host with preview

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from '@vuetify/vite-plugin';
 
-const path = require('path')
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,7 +26,7 @@ export default defineConfig({
       inline: ["vuetify"],
     },
     globals: true,
-  }
+  },
 
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
@@ -41,4 +41,4 @@ export default defineConfig({
     ]
   },
   */
-})
+});
