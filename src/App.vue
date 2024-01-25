95e56f32ba02b9604e0186557ff61e06cdb1ede4
Aidan Hibbard <aidanchibbard@gmail.com>
2024-01-24 18:25:45 -0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,11 @@
-<template>
-  <v-app :class="[darkMode ? 'darkMode' : '']">
-    <v-app-bar>
-      <v-icon 
-        color="blue lighten-2"
-        id="settings-toggle"
-        @click="drawer = !drawer"
-      >
-        mdi-cog-outline
-      </v-icon>
-      <v-spacer />
-      
-      <GMapAutocomplete
-        placeholder="Enter a place"
-        @place_changed="setCenter"
-      />
-      <v-btn 
-        primary
-        color="secondary"
-        small
-        @click="clearMarkers"
-        id="clearbtn"
-      >
-        Clear
-        <br> 
-        Map
-      </v-btn>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <Settings />
-    </v-navigation-drawer>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
-</template>
-
-<script>
-import { mapMutations, mapState } from 'vuex';
-import Settings from '@/components/Settings.vue';
-export default {
-  name: 'App',
-  components: {
-    Settings,
-  },
-  data: () => ({
-    drawer: false,
-    group: null,
-  }),
-  computed: {
-    ...mapState({
-      darkMode: (state) => state.darkMode,
-    }),
-  },
-  methods: {
-    ...mapMutations([
-      'setCenter',
-      'clearMarkers'
-    ]),
-  },
-};
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
-<style>
-/* Keep Google Autocomplete below Nav */
-.pac-container {
-  margin-top: 20px;
-}
-.pac-target-input {
-  border: 1px solid black;
-  border-radius: 3px;
-  max-width: 190px;
-  margin-right: 5px;
-}
-#clearbtn {
-  background-color: #dafaf7;
-}
-#settings-toggle {
-  margin-left: 10px;
-}
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
 </style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+import { vuetify } from './plugins/vuetify';
+
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(vuetify)
+  .mount('#app');
